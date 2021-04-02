32709a3e2bee1abf94074b5ce1f9a443bdb1457e
Draeconis <Draeconis@users.noreply.github.com>
2021-04-02 11:55:01 +0100

    nodejs integration, passedargs function

--- devicestechlead/app.asar/main.js
+++ devicestechlead/app.asar/main.js
@@ -1,19 +1,38 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const { ipcMain } = require('electron');
+var nodeConsole = require('console');
+var outputConsole = new nodeConsole.Console(process.stdout, process.stderr);
 
+const argv = process.argv.slice(1);
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: true,
+    resizable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      sandbox: true,
+      enableRemoteModule: false,
+      nodeIntegration: true
+    },
+    useContentSize: true,
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+global.passedargs = {}
+var slicedArgs = Array.prototype.slice.call(process.argv, 1).forEach(function(item) {items = item.split("="); global.passedargs[items[0]] = items[1] })
+
+if (global.passedargs['view'] == "main"){
+    mainWindow.loadFile('index.html')
+} else if (global.passedargs['view'] == "download"){
+    mainWindow.loadFile('download.html')
+} else {
+  app.quit()
+}
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,7 +43,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -41,3 +60,12 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.handle('quit-app', () => {
+    process.exit(0);
+});
+
+ipcMain.handle('start', () => {
+    outputConsole.log('start');
+    process.exit(0);
+});
