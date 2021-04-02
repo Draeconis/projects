3e416faeccfc13576d565604ee42e3d81cd810bd
Draeconis <Draeconis@users.noreply.github.com>
2021-04-02 15:44:39 +0100

    progress with windows python installer

---
+++ devicestechlead/script/prereq-win.ps1
@@ -0,0 +1,18 @@
+$is_64bit = [Environment]::Is64BitOperatingSystem
+$exeFile = "python.exe"
+$exeFilePath = "$env:appdata\$exeFile"
+
+if($is_64bit -eq $true) {
+  $url = ("https://www.python.org/ftp/python/3.9.2/python-3.9.2-amd64.exe")
+} else {
+  $url = ("https://www.python.org/ftp/python/3.9.2/python-3.9.2.exe")
+}
+
+Invoke-WebRequest -Uri $url -OutFile $
+if (!(Test-Path $exeFilePath)) {
+  Write-Host "error, download failed. exiting."
+  exit
+}
+Write-Host "Installing.."
+# https://docs.python.org/3.9/using/windows.html
+start-process -filepath $exeFilePath -argumentlist '/quiet', 'InstallAllUsers=1', 'PrependPath=1', 'Include_test=0', 'SimpleInstall=1'
