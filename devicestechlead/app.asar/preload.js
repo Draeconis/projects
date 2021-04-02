2d77ac9d96c17585540736b92508c39cf7247f13
Draeconis <Draeconis@users.noreply.github.com>
2021-04-02 11:55:23 +0100

    addEventListener events for button interaction

--- devicestechlead/app.asar/preload.js
+++ devicestechlead/app.asar/preload.js
@@ -1,12 +1,16 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const { ipcRenderer } = require('electron');
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+window.addEventListener('DOMContentLoaded', () => {
+  if (('#exitBtn').length > 0) {
+    document.getElementById("exitBtn").addEventListener("click", (e) => {
+        ipcRenderer.invoke('quit-app');
+    });
+  };
+  if (('#startBtn').length > 0) {
+    document.getElementById("startBtn").addEventListener("click", (e) => {
+        ipcRenderer.invoke('start');
+    });
+  };
 })
