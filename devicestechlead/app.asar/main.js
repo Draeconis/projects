57f0afe548256bd17c43bfbeef91695b045ef186
Geoff Smith <draeconis@icloud.com>
2021-04-04 18:12:39 +0100

    revert back to the python downloader, tell electron which file to monitor

--- devicestechlead/app.asar/main.js
+++ devicestechlead/app.asar/main.js
@@ -4,7 +4,7 @@ const path = require('path')
 const { ipcMain } = require('electron');
 var nodeConsole = require('console');
 var outputConsole = new nodeConsole.Console(process.stdout, process.stderr);
-import fs from 'fs';
+const fs = require("fs");
 
 const argv = process.argv.slice(1);
 function createWindow () {
@@ -72,77 +72,20 @@ ipcMain.handle('start', () => {
 });
 
 ipcMain.handle('download-progress', () => {
-  const sourceUrl = global.passedargs['inputpath'];
-  const targetFile = global.passedargs['outputpath'];
-  const length = global.passedargs['size'];
-  export default async function download(sourceUrl, targetFile, progressCallback, length) {
-  const request = new Request(sourceUrl, {
-    headers: new Headers({'Content-Type': 'application/octet-stream'})
-  });
+    // const sourceUrl = global.passedargs['inputpath'];
+    const targetFile = global.passedargs['outputpath'];
+    var length = global.passedargs['size'];
 
-  const response = await fetch(request);
-  if (!response.ok) {
-    throw Error(`Unable to download, server returned ${response.status} ${response.statusText}`);
-  }
-
-  const body = response.body;
-  if (body == null) {
-    throw Error('No response body');
-  }
-
-  const finalLength = length || parseInt(response.headers.get('Content-Length' || '0'), 10);
-  const reader = body.getReader();
-  const writer = fs.createWriteStream(targetFile);
-
-  await streamWithProgress(finalLength, reader, writer, progressCallback);
-  writer.end();
-}
-
-async function streamWithProgress(length, reader, writer, progressCallback) {
-  let bytesDone = 0;
-
-  while (true) {
-    const result = await reader.read();
-    if (result.done) {
-      if (progressCallback != null) {
-        progressCallback(length, 100);
-      }
-      return;
-    }
-
-    const chunk = result.value;
-    if (chunk == null) {
-      throw Error('Empty chunk received during download');
-    } else {
-      writer.write(Buffer.from(chunk));
-      if (progressCallback != null) {
-        bytesDone += chunk.byteLength;
-        const percent = length === 0 ? null : Math.floor(bytesDone / length * 100);
-        progressCallback(bytesDone, percent);
-      }
-    }
-  }
-}
+    fs.stat(targetFile, (err, fileStats) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (fileStats != fileMax) {
+                percentage = Math.ceil(dlfilesize / fileMax * 100)
+                return percentage;
+            } else {
+                process.exit(0);
+            }
+        }
+    })
 });
-
-
-// ipcMain.handle('watch-progress', () => {
-    // const dlfile = (global.passedargs['path']);
-    // const fileMax = (global.passedargs['size']);
-    // outputConsole.log(fileMax);
-    // return fileMax;
-
-    // if (dlfile.length > 0) {
-    //   const dlfilesizecheck = setInterval(function(){
-    //     const dlfilesize =
-    //     if (dlfilesize != fileMax) {
-    //       percentage = Math.ceil(dlfilesize / fileMax * 100)
-    //       return percentage;
-    //     } else {
-    //       clearInterval(dlfilesizecheck);
-    //     }
-    //       stuff
-    //     }
-    //   }, 500 );
-    // }
-// });
--- devicestechlead/app.asar/preload.js
+++ devicestechlead/app.asar/preload.js
@@ -3,26 +3,56 @@
 const { ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
-  if (('#exitBtn').length > 0) {
-    document.getElementById("exitBtn").addEventListener("click", (e) => {
-        ipcRenderer.invoke('quit-app');
-    });
-  };
-  if (('#startBtn').length > 0) {
-    document.getElementById("startBtn").addEventListener("click", (e) => {
-        ipcRenderer.invoke('start');
-    });
-  };
-  if (('#progressBar').length > 0) {
-    ipcRenderer.invoke('download-progress');
-    });
+    if (('#exitBtn').length > 0) {
+        document.getElementById("exitBtn").addEventListener("click", (e) => {
+            ipcRenderer.invoke('quit-app');
+        });
+    };
+    if (('#startBtn').length > 0) {
+        document.getElementById("startBtn").addEventListener("click", (e) => {
+            ipcRenderer.invoke('start');
+        });
+    };
+    if (('#progressBar').length > 0) {
+        const dlfilesizecheck = setInterval(function(){
+            ipcRenderer.invoke('download-progress').then((value) => {
+            });
+          } else {
+            clearInterval(dlfilesizecheck);
+        }
+        }, 500 );
+      }
+
   };
 });
 
+async function initialize() {
+    const dlfilesizecheck = setInterval(function(){
+        ipcRenderer.invoke('download-progress').then((value) => {
+            if ((value).length > 0) {
+                document.getElementById("progressBar").dosomething(val)
+            } else {
+                clearInterval(dlfilesizecheck)
+            }
+        });
+    }, 500 );
+}
 
-// if (('#progressBar').length > 0) {
-//   ipcRenderer.invoke('watch-progress').then((value) => {
-//     document.getElementById("progressBar").
-//     console.log(value);
-//   });
-// };
+initialize
+
+// function createFileDLCheckLoop() {
+//     const deferTimer = setInterval(function(){
+//         if ( ttl > 1 ) {
+//             ttl = ttl - 1;
+//             document.getElementById("globalTimer").innerHTML = (ttl);
+//             if ( ttl == 1 ) {
+//                 document.getElementById("mins").innerHTML = ("minute");
+//             }
+//         } else {
+//             outputConsole.log("1 Hour");
+//             clearInterval(deferTimer);
+//             var window = remote.getCurrentWindow();
+//             ipcRenderer.send('quit-app');
+//         }
+//     }, 60000 );
+// }
